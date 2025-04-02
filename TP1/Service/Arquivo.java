19f11ba71d69e5d5694d1e69225d302d02b6f17a
Cauã Costa Alves <91996777+cauacostaalves@users.noreply.github.com>
2025-04-02 13:44:41 -0300

    at

--- TP1/Service/Arquivo.java
+++ TP1/Service/Arquivo.java
@@ -22,12 +22,12 @@ public class Arquivo<T extends Registro>
 
     public Arquivo ( String na, Constructor<T> c ) throws Exception 
     {
-        File d = new File(".\\Codigo\\src\\main\\data");
+        File d = new File(".\\TP1");
         if ( !d.exists( ) ) {
             d.mkdir();
         } // end if
 
-        this.nomeArquivo = ".\\Codigo\\src\\main\\data\\" + na;
+        this.nomeArquivo = ".\\TP1" + na;
         this.construtor = c;
         arquivo = new RandomAccessFile(this.nomeArquivo, "rw");
         if (arquivo.length() < TAM_CABECALHO) {
